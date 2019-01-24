db2c7ab3b43060209ed4daca963447be05d8fea0
mkg1 <gehring.mg@gmail.com>
2019-01-23 23:51:57 -0500

    attribute brainstorm

--- eighthrs/models.py
+++ eighthrs/models.py
@@ -2,5 +2,16 @@ from django.db import models
 
 # Create your models here.
 class WorkDay(models.Model):
+    date
+    desk_timme
+    target_intervals
 class Profile(models.Model):
+    name = models.CharField(max_length=100)
+    height
+    weight
+
 class Exercise(models.Model):
+    ex_type
+    level
+    reps
+    sets
