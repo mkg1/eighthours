9fac25f9499d298fae1729e0b7e302380dfab5e1
mkg1 <gehring.mg@gmail.com>
2019-01-24 17:43:01 -0500

    Add data types and continue filling in schema

--- eighthrs/models.py
+++ eighthrs/models.py
@@ -2,16 +2,20 @@ from django.db import models
 
 # Create your models here.
 class WorkDay(models.Model):
-    date
-    desk_timme
-    target_intervals
+    date = models.DateTimeField()
+    desk_time = models.IntegerField()
+    target_intervals = models.IntegerField()
+    location = models.CharField(max_length=100)
+    description = models.TextField()
+
 class Profile(models.Model):
     name = models.CharField(max_length=100)
-    height
-    weight
+    email = models.CharField(max_length=100)
 
 class Exercise(models.Model):
-    ex_type
-    level
-    reps
-    sets
+    exercise_type = models.ManyToManyField('ExerciseType', blank=True)
+    reps = models.IntegerField()
+
+class Workout(models.Model):
+    work_day = models.OneToOneField('WorkDay')
+        
