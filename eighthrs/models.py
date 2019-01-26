7e7ae2086d3976210f58ca8738a4c3abc4f6dbec
mkg1 <gehring.mg@gmail.com>
2019-01-25 19:30:09 -0500

    Run initial migrations

--- eighthrs/models.py
+++ eighthrs/models.py
@@ -1,25 +1,25 @@
 from django.db import models
 
 # Create your models here.
-class WorkDay(models.Model):
-    date = models.DateTimeField()
-    desk_time = models.IntegerField()
-    target_intervals = models.IntegerField()
-    location = models.CharField(max_length=100)
-    description = models.TextField()
-    exercise = models.OneToManyField('Exercise')
-
+# class WorkDay(models.Model):
+#     date = models.DateTimeField()
+#     desk_time = models.IntegerField()
+#     target_intervals = models.IntegerField()
+#     location = models.CharField(max_length=100)
+#     description = models.TextField()
+#     exercise = models.OneToManyField('Exercise')
+#
 class Profile(models.Model):
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
-    work_day = models.OneToManyField('WorkDay')
-class Exercise(models.Model):
-    description = models.CharField(max_length=100)
-    exercise_type = models.ManyToManyField('ExerciseType', blank=True)
-    reps = models.IntegerField()
-
-class ExerciseType(models.Model):
-    description = models.CharField(max_length=50)
-
-class Workout(models.Model):
-    work_day = models.OneToOneField('WorkDay')
+    work_day = models.OneToManyFields('WorkDay')
+# class Exercise(models.Model):
+#     description = models.CharField(max_length=100)
+#     exercise_type = models.ManyToManyField('ExerciseType', blank=True)
+#     reps = models.IntegerField()
+#
+# class ExerciseType(models.Model):
+#     description = models.CharField(max_length=50)
+#
+# class Workout(models.Model):
+#     work_day = models.OneToOneField('WorkDay')
